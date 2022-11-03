00fee06aeb3dc5e4931f1f5cfda98bb02cbfc8ff
steve poling <sdpoling@gmail.com>
2022-11-02 21:44:30 -0400

    Initial commit

---
+++ CMakeFiles/chronos.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chronos.dir/src/chronos.cpp.o"
+  "CMakeFiles/chronos.dir/src/chronos.cpp.o.d"
+  "chronos"
+  "chronos.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chronos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/steve/Projects/chronos
+# Build directory: /Users/steve/Projects/chronos
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
