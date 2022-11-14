c67df961e701d9b3469aea981113b7ed83cd97ef
Steve Poling <sdpoling@gmail.com>
2022-11-14 13:23:51 -0500

    Support x86 platform

--- CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/steve/Projects/chronos
-# Build directory: /Users/steve/Projects/chronos
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
